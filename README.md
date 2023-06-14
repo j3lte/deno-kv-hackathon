403cd8ceee34f76f71bcd530a88a8a902437592f
j3lte <jwlagendijk@gmail.com>
2023-06-14 05:04:33 +0200

    First setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Deno KV Hackathon - ExchangeSecrets
+
+### Description
+
+Project for the Deno KV Hackathon.
+
+### Usage
+
+Start the project:
+
+```
+deno task start
+```
+
+This will watch the project directory and restart as necessary.
---
+++ routes/index.tsx
@@ -0,0 +1,14 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Exchange Secrets</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+
+      </div>
+    </>
+  );
+}
