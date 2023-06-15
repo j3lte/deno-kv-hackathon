f14bb75ae3981ed6696c7d02b86b247f870bc8ee
j3lte <jwlagendijk@gmail.com>
2023-06-15 11:43:52 +0200

    Cleanup imports

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -1,5 +1,5 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
-import { SessionState } from "../utils/types.ts";
+import { SessionState } from "@utils/types.ts";
 import { getSessionId } from "kv_oauth";
 
 export async function handler(
